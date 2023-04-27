9ec217e5f58785290ca90ad025eac7cfb45afdd6
Vinamra Mahajan <104397548+vinamra-M@users.noreply.github.com>
2023-04-27 18:54:33 +0530

    Create 392.java

---
+++ 392.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int l = 0;
+        if(s.length() == 0){
+            return true;
+        }
+        for(int i = 0; i < t.length();i++){
+            if(s.charAt(l) == t.charAt(i)){
+                l++;
+            }
+              if(l == s.length()){
+            return true;
+        }
+
+        }
+
+        return false;
+    }
+}
