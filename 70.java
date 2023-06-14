3d8850015db64bd6301b78d2858f3f536f1a4912
Vinamra Mahajan <104397548+vinamra-M@users.noreply.github.com>
2023-06-15 02:22:25 +0530

    Create 70.java

---
+++ 70.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+
+        int x = 1;
+        int y = 2;
+        for(int i = 2; i < n; i++)
+        {
+            int temp = x;
+            x = y;
+            y = temp + y;
+        }
+        return y;
+        
+    }
+}
