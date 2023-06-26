76d1c7bfc8b81f4a0879ad1367e32c54b1eeb016
Vinamra Mahajan <104397548+vinamra-M@users.noreply.github.com>
2023-06-26 10:23:32 +0530

    Create 75.java

---
+++ 75.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int l = 0;
+        int r = nums.length-1;
+        int i = 0;
+
+        while(i <= r){
+            if(nums[i] == 0) swap(nums,i++,l++);
+
+            else if(nums[i] == 1) i++;
+
+            else swap(nums, i, r--); 
+        }
+        
+    }
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
