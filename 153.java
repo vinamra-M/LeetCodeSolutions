ce4b9001db0f3ec7673b7eb822c908b1864ed988
Vinamra Mahajan <104397548+vinamra-M@users.noreply.github.com>
2023-06-27 16:24:41 +0530

    Create 153.java

---
+++ 153.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findMin(int[] arr) {
+        int pivot = findPivot(arr);
+        return arr[pivot+1];
+        
+    }
+
+     int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+}
