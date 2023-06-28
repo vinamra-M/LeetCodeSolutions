5bfe0437dcec4b31f2a9e1e23c757b79d8ddbaf0
Vinamra Mahajan <104397548+vinamra-M@users.noreply.github.com>
2023-06-28 17:24:09 +0530

    Create 204.java

---
+++ 204.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n == 0 || n == 1) return 0;
+
+        int count = 0;
+
+        boolean prime[] = new boolean[n + 1];
+        for (int i = 0; i <= n; i++)
+            prime[i] = true;
+        
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p] == true) {
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+         for (int i = 2; i < n; i++) {
+            if (prime[i] == true)
+                count++;
+        }
+        return count;
+    }
+}
