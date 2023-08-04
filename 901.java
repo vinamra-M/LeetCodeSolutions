c92d17c963783be4d806cec679c0f1220e13bb21
Vinamra Mahajan <104397548+vinamra-M@users.noreply.github.com>
2023-08-04 19:55:10 +0530

    Create 901.java

---
+++ 901.java
@@ -0,0 +1,21 @@
+class StockSpanner {
+    Stack<int[]> stack;
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int res = 1;
+        while(!stack.isEmpty() && stack.peek()[1] <= price){
+            res += stack.pop()[0];
+        }
+        stack.push(new int[]{res,price});
+        return res;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
